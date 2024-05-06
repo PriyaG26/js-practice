f7dbe05fa543799f98603497b5d03af6b9feb3d3
PriyaG26 <90601805+PriyaG26@users.noreply.github.com>
2024-05-06 16:35:04 +0000

    Lec 5 done

---
+++ Basics/datatypes05.js
@@ -0,0 +1,19 @@
+// for the newer version of JS, tho not necessary but a good practice
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) //not usable like this, gives error since we use node js, works in browser -> inspect -> console
+
+let name = "priya" //string => "..."
+let age = 20 //number Range -> 2^53
+let isLoggedIn = true //boolean -> true/false
+
+// bigint for very big numbers like in stock market, but not for mornal use
+// null -> standalone value/type, representation of empty value
+// undefined -> when the value is not defined, value is not yet assigned
+// symbol -> for identifying uniqueness 
+//object -> 
+
+console.log(typeof "priya")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
